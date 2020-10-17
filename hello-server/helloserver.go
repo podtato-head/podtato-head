fe8d84e20410db968b08a76c60d98031c5a27fe8
Alois Reitbauer <alois@reitbauer.cc>
2020-10-17 15:11:48 +0200

    Initial commit

---
+++ hello-server/helloserver.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// create a handler struct
+type HttpHandler struct{}
+
+// implement `ServeHTTP` method on `HttpHandler` struct
+func (h HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+
+	// create response binary data
+	data := []byte("Hello World!") // slice of bytes
+
+	// write `data` to response
+	res.Write(data)
+}
+
+func main() {
+
+	// create a new handler
+	handler := HttpHandler{}
+
+	fmt.Println("Serving requests on port 9000")
+	// listen and serve
+	http.ListenAndServe(":9000", handler)
+
+}
