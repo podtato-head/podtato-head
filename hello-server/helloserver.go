4c9105eb242ba1d4d064b3389856e79e457b54fa
Alois Reitbauer <alois@reitbauer.cc>
2020-10-21 15:52:41 +0200

    updates builds and server to serve static content

--- hello-server/helloserver.go
+++ hello-server/helloserver.go
@@ -11,25 +11,25 @@ var version string
 // create a handler struct
 type HTTPHandler struct{}
 
-	// implement `ServeHTTP` method on `HttpHandler` struct
-	func (h HTTPHandler) ServeHTTP(res http.ResponseWriter, req *http.Request){ 
+// implement `ServeHTTP` method on `HttpHandler` struct
+func (h HTTPHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 
-		fmt.Fprintf(res, "<html><head><title>Hello server</title></head><body>")
-		fmt.Fprintf(res, "<h1>Hello World! - Version %s </h1>", version)
-		fmt.Fprintf(res, "</body</html>")
-	}
+	fmt.Fprintf(res, "<html><head><title>Hello server</title></head><body>")
+	fmt.Fprintf(res, "<h1>Hello World! - Version %s </h1>", version)
+	fmt.Fprintf(res, "</body</html>")
+}
 
 func main() {
 
 	// expecting version as first parameter
 	version = os.Args[1]
 
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle ("/static/", fs)
-	// create a new handler
-	handler := HTTPHandler{} 
-	http.Handle ("/", handler)
+	fileServer := http.FileServer(http.Dir("./static"))
 
+	// create a new handler
+	handler := HTTPHandler{}
+	http.Handle("/", handler)
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
 	fmt.Println("Serving requests on port 9000")
 	// listen and serve
 	http.ListenAndServe(":9000", nil)
