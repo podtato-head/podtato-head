725d7c2dcfcc71d68feba0039b6c29b6203c076c
Alois Reitbauer <alois@reitbauer.cc>
2020-10-21 07:45:27 +0200

    adjusted build process

--- hello-server/buildPush.sh
+++ hello-server/buildPush.sh
@@ -1,5 +1,18 @@
+#!/usr/bin/env bash
 
-tag=$1
+REPOSITORY="aloisreitbauer"
 
-docker build . -t aloisreitbauer/hello-server:$tag
-docker push aloisreitbauer/hello-server:$tag  
+TAG="0.1.0"
+
+docker build -f ./DockerfileV"${TAG}" . --tag "${REPOSITORY}"/hello-server:v"${TAG}"
+docker push "${REPOSITORY}"/hello-server:v"${TAG}"  
+
+TAG="0.1.1"
+
+docker build -f ./DockerfileV"${TAG}" . --tag "${REPOSITORY}"/hello-server:v"${TAG}"
+docker push "${REPOSITORY}"/hello-server:v"${TAG}"  
+
+TAG="0.1.2"
+
+docker build -f ./DockerfileV"${TAG}" . --tag "${REPOSITORY}"/hello-server:v"${TAG}"
+docker push "${REPOSITORY}"/hello-server:v"${TAG}"  
--- hello-server/helloserver.go
+++ hello-server/helloserver.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
+
+var version string
 
 // create a handler struct
 type HttpHandler struct{}
@@ -12,14 +15,17 @@ type HttpHandler struct{}
 	func (h HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request){ 
 
 		fmt.Fprintf(res, "<html><head><title>Hello server</title></head><body>")
-		fmt.Fprintf(res, "<h1>Hello World! - Version 0.1.0</h1>")
+		fmt.Fprintf(res, "<h1>Hello World! - Version %s </h1>", version)
 		fmt.Fprintf(res, "</body</html>")
 	}
 
 func main() {
 
+	// expecting version as first parameter
+	version = os.Args[1]
+
 	// create a new handler
-	handler := HttpHandler{}
+	handler := HttpHandler{} 
 
 	fmt.Println("Serving requests on port 9000")
 	// listen and serve
