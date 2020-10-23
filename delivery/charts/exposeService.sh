73a396d89b5de7a45b74943c1101ccef52852dca
Alois Reitbauer <alois@reitbauer.cc>
2020-10-23 11:45:25 +0200

    create helper script

---
+++ delivery/charts/exposeService.sh
@@ -0,0 +1,4 @@
+export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=hello-server,app.kubernetes.io/instance=kubecon-release" -o jsonpath="{.items[0].metadata.name}")
+  export CONTAINER_PORT=$(kubectl get pod --namespace default $POD_NAME -o jsonpath="{.spec.containers[0].ports[0].containerPort}")
+  echo "Visit http://127.0.0.1:8080 to use your application"
+  kubectl --namespace default port-forward $POD_NAME 8080:$CONTAINER_PORT
