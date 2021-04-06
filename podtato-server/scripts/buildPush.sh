73542d5e398d27d77e10b3228f05c285ef33c282
Alex <alexsimonjones@gmail.com>
2021-04-06 16:29:43 +0100

    moved the static path in the dockerfile for building

--- podtato-server/scripts/buildPush.sh
+++ podtato-server/scripts/buildPush.sh
@@ -4,7 +4,6 @@ REPOSITORY="ghcr.io/podtato-head"
 declare -a TAGS=("0.1.0" "0.1.1" "0.1.2")
 for TAG in "${TAGS[@]}"
 do
-  pwd
   docker build -f docker/DockerfileV"${TAG}" . --tag "${REPOSITORY}"/podtatoserver:v"${TAG}"
   docker push "${REPOSITORY}"/podtatoserver:v"${TAG}"
 done
