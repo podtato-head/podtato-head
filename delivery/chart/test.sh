4223da36e60a0348b2b974f11963ce692b0e10d1
Josh Gavant <joshgavant@gmail.com>
2021-09-01 17:16:59 -0500

    fixes

--- delivery/chart/test.sh
+++ delivery/chart/test.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+set -e
+
 github_user=${1:-cncf}
 github_token=${2}
 # ci_version=${3:-latest-dev}
@@ -7,17 +9,19 @@ github_token=${2}
 this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
 namespace=podtato-helm
-kubectl create namespace ${namespace} &> /dev/null
+kubectl create namespace ${namespace} --save-config || true &> /dev/null
 kubectl config set-context --current --namespace=${namespace}
 
-kubectl create secret docker-registry ghcr \
-    --docker-server 'https://ghcr.io/' \
-    --docker-username "${github_user}" \
-    --docker-password "${github_token}"
+if [[ -n "${github_token}" ]]; then
+    kubectl create secret docker-registry ghcr \
+        --docker-server 'https://ghcr.io/' \
+        --docker-username "${github_user}" \
+        --docker-password "${github_token}"
+fi
 
-helm install --debug podtato-head ${this_dir} \
+helm upgrade --install --debug podtato-head ${this_dir} \
     --set "images.repositoryDirname=ghcr.io/${github_user}/podtato-head" \
-    --set "images.pullSecrets[0].name=ghcr"
+    ${github_token:+--set "images.pullSecrets[0].name=ghcr"}
 
 echo ""
 echo "----> main deployment:"
