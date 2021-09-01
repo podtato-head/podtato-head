eeede52471a70d8c2bee8af268b31babb16035b6
Josh Gavant <joshgavant@gmail.com>
2021-09-01 16:13:16 -0500

    use ghcr secret in kind cluster

--- delivery/chart/test.sh
+++ delivery/chart/test.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 
 github_user=${1:-cncf}
-# ci_version=${2:-latest-dev}
+github_token=${2}
+# ci_version=${3:-latest-dev}
 
 this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
@@ -9,8 +10,14 @@ namespace=podtato-helm
 kubectl create namespace ${namespace} &> /dev/null
 kubectl config set-context --current --namespace=${namespace}
 
+kubectl create secret docker-registry ghcr \
+    --docker-server 'https://ghcr.io/' \
+    --docker-username "${github_user}" \
+    --docker-password "${github_token}"
+
 helm install --debug podtato-head ${this_dir} \
-    --set "images.repositoryDirname=ghcr.io/${github_user}/podtato-head"
+    --set "images.repositoryDirname=ghcr.io/${github_user}/podtato-head" \
+    --set "images.pullSecrets[0].name=ghcr"
 
 echo ""
 echo "----> main deployment:"
--- delivery/kubectl/test.sh
+++ delivery/kubectl/test.sh
@@ -1,12 +1,21 @@
 #! /usr/bin/env bash
 
 github_user=${1:-cncf}
-ci_version=${2:-latest-dev}
+github_token=${2}
+ci_version=${3:-latest-dev}
 
 echo "ci_version: ${ci_version}, github_user: ${github_user}"
 
 this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 namespace=podtato-kubectl
+
+kubectl create secret docker-registry ghcr \
+    --docker-server 'https://ghcr.io/' \
+    --docker-username "${github_user}" \
+    --docker-password "${github_token}"
+
+kubectl patch serviceaccount default \
+    --patch '{ "imagePullSecrets": [{ "name": "ghcr" }]}'
 
 cat "${this_dir}/manifest.yaml" | \
     sed "s@ghcr\.io\/podtato-head@ghcr.io/${github_user}/podtato-head@g" | \
