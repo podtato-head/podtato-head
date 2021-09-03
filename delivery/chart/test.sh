b59b16227ce071db9b30a0a07c6f20ae8ccd071d
Josh Gavant <4421720+joshgav@users.noreply.github.com>
2021-09-03 02:06:41 -0500

    use custom secret for main container build, add pull secret to test cluster (#95)
    * use custom secret for main container build
    
    * use ghcr secret in kind cluster
    
    * fixes

--- delivery/chart/test.sh
+++ delivery/chart/test.sh
@@ -1,16 +1,27 @@
 #! /usr/bin/env bash
 
+set -e
+
 github_user=${1:-cncf}
-# ci_version=${2:-latest-dev}
+github_token=${2}
+# ci_version=${3:-latest-dev}
 
 this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
 namespace=podtato-helm
-kubectl create namespace ${namespace} &> /dev/null
+kubectl create namespace ${namespace} --save-config || true &> /dev/null
 kubectl config set-context --current --namespace=${namespace}
 
-helm install --debug podtato-head ${this_dir} \
-    --set "images.repositoryDirname=ghcr.io/${github_user}/podtato-head"
+if [[ -n "${github_token}" ]]; then
+    kubectl create secret docker-registry ghcr \
+        --docker-server 'https://ghcr.io/' \
+        --docker-username "${github_user}" \
+        --docker-password "${github_token}"
+fi
+
+helm upgrade --install --debug podtato-head ${this_dir} \
+    --set "images.repositoryDirname=ghcr.io/${github_user}/podtato-head" \
+    ${github_token:+--set "images.pullSecrets[0].name=ghcr"}
 
 echo ""
 echo "----> main deployment:"
--- delivery/kubectl/test.sh
+++ delivery/kubectl/test.sh
@@ -1,18 +1,34 @@
 #! /usr/bin/env bash
 
+set -e
+
 github_user=${1:-cncf}
-ci_version=${2:-latest-dev}
+github_token=${2}
+ci_version=${3:-latest-dev}
 
 echo "ci_version: ${ci_version}, github_user: ${github_user}"
 
 this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 namespace=podtato-kubectl
+kubectl create namespace ${namespace} --save-config || true &> /dev/null
+kubectl config set-context --current --namespace ${namespace}
+
+if [[ -n "${github_token}" ]]; then
+    kubectl create secret docker-registry ghcr \
+        --docker-server 'https://ghcr.io/' \
+        --docker-username "${github_user}" \
+        --docker-password "${github_token}"
+
+    kubectl patch serviceaccount default \
+        --patch '{ "imagePullSecrets": [{ "name": "ghcr" }]}'
+fi
 
 cat "${this_dir}/manifest.yaml" | \
     sed "s@ghcr\.io\/podtato-head@ghcr.io/${github_user}/podtato-head@g" | \
     sed "s/latest-dev/${ci_version}/g" | \
         kubectl apply -f -
 
-kubectl wait --for=condition=Available deployment --namespace ${namespace} podtato-main
+kubectl wait --for=condition=Available --timeout=90s \
+    deployment --namespace ${namespace} podtato-main
 
 kubectl get deployments --namespace=${namespace}
