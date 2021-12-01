a10b881b6cd7997814defc5c43d6c0788961e67b
Josh Gavant <joshgavant@gmail.com>
2021-12-01 15:56:15 -0600

    automate version determination

--- podtato-head/build/build_images.sh
+++ podtato-head/build/build_images.sh
@@ -1,39 +1,67 @@
 #! /usr/bin/env bash
 
-# variables
+# configurable env vars:
+# GITHUB_USER
+# GITHUB_TOKEN
+# COSIGN_KEY_PATH
+# COSIGN_PASSWORD
+# VERSION: if set, override default version. Default version is previous tag for non-main branches, incremented tag for main
+# RELEASE_BUILD: if set, push to ghcr.io/podtato-head, increment version from last tag, and possibly apply a git tag
+# INCREMENT_MAJOR: if set, increment major on release (default is to increment minor)
+# INCREMENT_PATCH: if set, increment patch (x.y.Z) on release (default is to increment minor)
+# PUSH_TO_REGISTRY: if set, push image to remote registry after building locally
+
+### set up build
+
+# set common variables
 declare -r this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 declare -r app_dir=$(cd ${this_dir}/.. && pwd)
 declare -r root_dir=$(cd ${this_dir}/../.. && pwd)
-if [[ -e ${root_dir}/.env ]]; then source ${root_dir}/.env; fi
+if [[ -e ${root_dir}/.env ]]; then 
+    echo "INFO: sourcing env vars from .env in repo root"
+    source ${root_dir}/.env
+fi
+# /end set common variables
 
-version='0.1.0'
+# set up registry access
 registry=ghcr.io
 github_user=${1:-${GITHUB_USER}}
 github_token=${2:-${GITHUB_TOKEN}}
+commit_sha=$(git rev-parse HEAD)
 
-sign_images=$(if type -P cosign > /dev/null; then echo 1; else echo 0; fi)
-cosign_key_path=${COSIGN_KEY_PATH:-"${root_dir}/.github/workflows/cosign.key"}
-git_sha=$(git rev-parse HEAD)
-
-if [[ ${sign_images} == 1 && -n "${COSIGN_PASSWORD}" ]]; then
-    echo "INFO: will sign images using cosign version $(cosign version --json | jq -r '.GitVersion')"
-fi
-
+# sign in if possible
 if [[ -z "${github_token}" ]]; then
     echo "WARNING: GITHUB_TOKEN not set, push may fail"
 else
     echo "${github_token}" | docker login --username=${github_user} --password-stdin ${registry} &> /dev/null
 fi
-# /variables
+# /end set up registry access
 
+# set version/tag for this build
+source ${this_dir}/version.sh
+version=$(version_to_use)
+echo "INFO: will use version/tag: ${version}"
+# /end set version
+
+# set up cosign
+sign_images=$(if type -P cosign > /dev/null; then echo 1; else echo 0; fi)
+cosign_key_path=${COSIGN_KEY_PATH:-"${root_dir}/.github/workflows/cosign.key"}
+if [[ ${sign_images} == 1 && -n "${COSIGN_PASSWORD}" ]]; then
+    echo "INFO: will sign images using cosign version $(cosign version --json | jq -r '.GitVersion')"
+fi
+# /end set up cosign
+
+### /end set up build
+
+### build entry container
 if [[ -z "${RELEASE_BUILD}" ]]; then
     container_name=${registry}/${github_user}/podtato-head/entry
 else
     container_name=${registry}/podtato-head/entry
 fi
 
-echo "building container for entry as ${container_name}"
 echo ""
+echo "INFO: building container for entry as ${container_name}"
 docker build ${app_dir} \
     --tag "${container_name}:latest" \
     --tag "${container_name}:${version}" \
@@ -44,20 +72,15 @@ if [[ -n "${PUSH_TO_REGISTRY}" ]]; then
     docker push "${container_name}:${version}"
 fi
 if [[ ${sign_images} == 1 && -n "${COSIGN_PASSWORD}" ]]; then
-    echo "signing ${container_name}"
-    echo ""
+    echo "INFO: signing ${container_name}"
     cosign sign --key ${cosign_key_path} \
-        --annotations git_sha=${git_sha} \
-        --annotations version=latest \
+        --annotations git_commit=${git_commit} \
         --annotations version=${version} \
         ${container_name}:latest
-
-    cosign sign --key ${cosign_key_path} \
-        --annotations git_sha=${git_sha} \
-        --annotations version=${version} \
-        ${container_name}:${version}
 fi
+### /end build entry container
 
+### build parts containers
 parts=($(find ${app_dir}/pkg/assets/images/* -type d -printf '%f\n'))
 for part in "${parts[@]}"; do
     if [[ -z "${RELEASE_BUILD}" ]]; then
@@ -65,8 +88,8 @@ for part in "${parts[@]}"; do
     else
         container_name=${registry}/podtato-head/${part}
     fi
-    echo "building container for ${part} as ${container_name}"
     echo ""
+    echo "INFO: building container for ${part} as ${container_name}"
     docker build ${app_dir} \
         --tag "${container_name}:latest" \
         --tag "${container_name}:${version}" \
@@ -78,17 +101,11 @@ for part in "${parts[@]}"; do
         docker push "${container_name}:${version}"
     fi
     if [[ "${sign_images}" == 1 && -n "${COSIGN_PASSWORD}" ]]; then
-        echo "signing ${container_name}"
-        echo ""
+        echo "INFO: signing ${container_name}"
         cosign sign --key ${cosign_key_path} \
-            --annotations git_sha=${git_sha} \
-            --annotations version=latest \
-            --annotations version=${version} \
-            ${container_name}:latest
-
-        cosign sign --key ${cosign_key_path} \
-            --annotations git_sha=${git_sha} \
+            --annotations git_commit=${git_commit} \
             --annotations version=${version} \
             ${container_name}:${version}
     fi
 done
+### /end build parts containers
