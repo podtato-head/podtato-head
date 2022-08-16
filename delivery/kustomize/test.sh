8e8b1beea0f56984eb10ceebff097524911415ef
michael <mxnxpx@gmail.com>
2022-08-16 09:44:56 -0400

    updating kustomize script copy pasta error

--- delivery/kustomize/test.sh
+++ delivery/kustomize/test.sh
@@ -7,7 +7,7 @@ if [[ -f "${root_dir}/.env" ]]; then source "${root_dir}/.env"; fi
 source ${root_dir}/scripts/registry-secrets.sh
 
 github_user=${1:-${GITHUB_USER}}
-github_token=${2:-${GITHUB_TOKEN}}
+github_user_lower=${github_user,,}
 github_token=${2:-${GITHUB_TOKEN}}
 
 image_version=$(${root_dir}/podtato-head-microservices/build/image_version.sh)
